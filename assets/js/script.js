821fc56e0936f1e981ae642029943047194501fa
tpikachu <46133358+tpikachu@users.noreply.github.com>
2022-04-27 09:49:03 -0700

    email address fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,8 +164,9 @@ contactForm.onsubmit = function(e) {
   e.preventDefault();
   // const fullName = document.contactForm.fullname.value;
   // const email = document.contactForm.email.value;
+  const receiver = 'lwc.michael7@gmail.com';
   const message = document.contactForm.message.value;
   const subject = document.contactForm.subject.value;
 
-  window.open(`mailto:lwc.michael7@gmail.com?subject=${subject}&body=${message}`, "__blank");
+  window.open(`mailto:lwc.michael7@gmail.com?to=${receiver}&subject=${subject}&body=${message}`, "__blank");
 }
